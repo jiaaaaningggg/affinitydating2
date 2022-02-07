5a3d334125ae272785f6d02bbba70e7524051432
Jiaaaaningggg <S10205022@connect.np.edu.sg>
2022-02-07 10:35:25 +0800

    Initial Commit

---
+++ affinitydating2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  affinitydating2
+//
+//  Created by Jia Ning Tjong on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
